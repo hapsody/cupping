b83baf397a3a8d93c90371f33ca4d89bbebb7ac8
hapsody <hapsody@gmail.com>
2018-05-07 10:05:02 +0900

    add graph

--- history.php
+++ history.php
@@ -1,10 +1,30 @@
 <?php
 session_start();
 
+function write_arr_to_script($target_arr)
+{
+  $i = 0;
+  $arr_length = count($target_arr);
+  echo '[';
+  foreach($target_arr as $item)
+  {
+    echo $item;
+    $i++;
+    if($i != $arr_length)
+    {
+      echo ',';
+    }
+  }
+  echo '],';
+  return;
+}
+
 $conn = mysqli_connect("localhost", "root", "11182917", "cupping");
 if (!$conn) die ('DB Error');
 $check = "SELECT * from taste";
 $result = $conn->query($check);
+
+
 
 ?>
 
@@ -21,140 +41,34 @@ $result = $conn->query($check);
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
   <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/darkly/bootstrap.min.css" rel="stylesheet" integrity="sha384-ae362vOLHy2F1EfJtpMbNW0i9pNM1TP2l5O4VGYYiLJKsaejqVWibbP6BSf0UU5i" crossorigin="anonymous">
-  <style type="text/css">
-  .marketing .col-lg-4 {
-    margin-bottom: 1.5rem;
-    text-align: center;
-  }
-
-  .taste-pics{
-    height:140px;
-    width:140px;
-    margin:auto;
-    background:#707070;
-  }
-
-  input[type=range] {
-    -webkit-appearance: none;
-    width: 50%;
-    margin: 2.45px 0;
-  }
-  input[type=range]:focus {
-    outline: none;
-  }
-  input[type=range]::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 3.1px;
-    cursor: pointer;
-    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0), 0px 0px 1px rgba(13, 13, 13, 0);
-    background: #A65700;
-    border-radius: 22.9px;
-    border: 0.5px solid #010101;
-  }
-  input[type=range]::-webkit-slider-thumb {
-    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.31), 0px 0px 1px rgba(13, 13, 13, 0.31);
-    border: 0px solid #000000;
-    height: 8px;
-    width: 16px;
-    border-radius: 3px;
-    background: #ffffff;
-    cursor: pointer;
-    -webkit-appearance: none;
-    margin-top: -2.95px;
-  }
-  input[type=range]:focus::-webkit-slider-runnable-track {
-    background: #e37900;
-  }
-  input[type=range]::-moz-range-track {
-    width: 100%;
-    height: 3.1px;
-    cursor: pointer;
-    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0), 0px 0px 1px rgba(13, 13, 13, 0);
-    background: #5e3200;
-    border-radius: 22.9px;
-    border: 0.5px solid #010101;
-  }
-  input[type=range]::-moz-range-thumb {
-    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.31), 0px 0px 1px rgba(13, 13, 13, 0.31);
-    border: 0px solid #000000;
-    height: 8px;
-    width: 16px;
-    border-radius: 3px;
-    background: #ffffff;
-    cursor: pointer;
-  }
-  input[type=range]::-ms-track {
-    width: 100%;
-    height: 3.1px;
-    cursor: pointer;
-    background: transparent;
-    border-color: transparent;
-    color: transparent;
-  }
-  input[type=range]::-ms-fill-lower {
-    background: #000000;
-    border: 0.5px solid #010101;
-    border-radius: 45.8px;
-    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0), 0px 0px 1px rgba(13, 13, 13, 0);
-  }
-  input[type=range]::-ms-fill-upper {
-    background: #5e3200;
-    border: 0.5px solid #010101;
-    border-radius: 45.8px;
-    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0), 0px 0px 1px rgba(13, 13, 13, 0);
-  }
-  input[type=range]::-ms-thumb {
-    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.31), 0px 0px 1px rgba(13, 13, 13, 0.31);
-    border: 0px solid #000000;
-    height: 8px;
-    width: 16px;
-    border-radius: 3px;
-    background: #ffffff;
-    cursor: pointer;
-    height: 3.1px;
-  }
-  input[type=range]:focus::-ms-fill-lower {
-    background: #5e3200;
-  }
-  input[type=range]:focus::-ms-fill-upper {
-    background: #e37900;
-  }
-
-  </style>
-
 
   <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
   <script src="js/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
   <script src="js/custom.js"></script>
 
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script type="text/javascript" src="js/jquery.tablesorter.min.js"></script>
 
+  <link rel="stylesheet" type="text/css" href="css/jquery.jqplot.min.css" />
+  <script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
 
   <script>
+  $(document).ready(function()
+  {
+    $("#historytable").tablesorter();
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-23019901-1']);
-  _gaq.push(['_setDomainName', "bootswatch.com"]);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
+    //1번째 입력값 (그려진 영역의 id값)
+    //2번째 입력값 (그래프내에 들어갈 데이터 값)
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+
+  });
 
 </script>
+
 </head>
 
 <body>
 
-
-  <div id="saving_effect" class="alert alert-dismissible alert-primary">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <strong>Data Saved</strong>
-  </div>
 
   <!-- head -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -165,10 +79,10 @@ $result = $conn->query($check);
 
     <div class="collapse navbar-collapse" id="navbarColor01">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
+        <li class="nav-item">
           <a class="nav-link" href="index.php">Evalutation <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item active">
           <a class="nav-link" href="history.php">History</a>
         </li>
 
@@ -201,68 +115,125 @@ $result = $conn->query($check);
 
   <!-- Three columns of text below the carousel -->
   <div class="container marketing">
-    <form action="submit_process.php" method="post">
+    <div class="row"> <!-- row 1-->
+      <table id="historytable" class="table table-hover" style="width:500px;height:500px">
+        <thead>
+          <tr>
+            <th scope="col">DateTime</th>
+            <th scope="col">Flavor</th>
+            <th scope="col">Balance</th>
+            <th scope="col">Roasting</th>
+            <th scope="col">Acidity</th>
+            <th scope="col">Sweetness</th>
+            <th scope="col">Aroma</th>
+            <th scope="col">aftertaste</th>
+            <th scope="col">Uniformity</th>
+            <th scope="col">Cleanup</th>
+            <th scope="col">Defect</th>
+            <th scope="col">Body</th>
+            <th scope="col">Avg</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
 
+          //while($i < $result->num_rows)
+          $i = 0;
+          while($row = $result->fetch_array(MYSQLI_ASSOC))
+          {
 
-      <div class="row"> <!-- row 1-->
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">DateTime</th>
-              <th scope="col">Flavor</th>
-              <th scope="col">Balance</th>
-              <th scope="col">Roasting</th>
-              <th scope="col">Acidity</th>
-              <th scope="col">Sweetness</th>
-              <th scope="col">Aroma</th>
-              <th scope="col">aftertaste</th>
-              <th scope="col">Uniformity</th>
-              <th scope="col">Cleanup</th>
-              <th scope="col">Defect</th>
-              <th scope="col">Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-
-            //while($i < $result->num_rows)
-            $i = 0;
-            while($row = $result->fetch_array(MYSQLI_ASSOC))
-            {
-
-              if ($i % 2 == 0){
-                echo '<tr class="table-active">';
-              } else {
-                echo '<tr>';
-              }
-              echo '<th scope="row">'.$row['created'].'</th>';
-              echo '<th scope="col">'.$row['flavor'].'</th>';
-              echo '<th scope="col">'.$row['balance'].'</th>';
-              echo '<th scope="col">'.$row['roastlevel'].'</th>';
-              echo '<th scope="col">'.$row['acidity'].'</th>';
-              echo '<th scope="col">'.$row['sweetness'].'</th>';
-              echo '<th scope="col">'.$row['aroma'].'</th>';
-              echo '<th scope="col">'.$row['aftertaste'].'</th>';
-              echo '<th scope="col">'.$row['uniformity'].'</th>';
-              echo '<th scope="col">'.$row['cleanup'].'</th>';
-              echo '<th scope="col">'.$row['defect'].'</th>';
-              echo '<th scope="col">'.$row['body'].'</th>';
-              echo '</tr>';
-              $i = $i + 1;
+            if ($i % 2 == 0){
+              echo '<tr class="table-active">';
+            } else {
+              echo '<tr>';
             }
-            ?>
+            echo '<th scope="row">'.$row['created'].'</th>';
+            $created_arr[$i] = $row['created'];
+            echo '<th scope="col">'.$row['flavor'].'</th>';
+            $flavor_arr[$i] = $row['flavor'];
+            echo '<th scope="col">'.$row['balance'].'</th>';
+            $balance_arr[$i] = $row['balance'];
+            echo '<th scope="col">'.$row['roastlevel'].'</th>';
+            $roastlevel_arr[$i] = $row['roastlevel'];
+            echo '<th scope="col">'.$row['acidity'].'</th>';
+            $acidity_arr[$i] = $row['acidity'];
+            echo '<th scope="col">'.$row['sweetness'].'</th>';
+            $sweetness_arr[$i] = $row['sweetness'];
+            echo '<th scope="col">'.$row['aroma'].'</th>';
+            $aroma_arr[$i] = $row['aroma'];
+            echo '<th scope="col">'.$row['aftertaste'].'</th>';
+            $aftertaste_arr[$i] = $row['aftertaste'];
+            echo '<th scope="col">'.$row['uniformity'].'</th>';
+            $uniformity_arr[$i] = $row['uniformity'];
+            echo '<th scope="col">'.$row['cleanup'].'</th>';
+            $cleanup_arr[$i] = $row['uniformity'];
+            echo '<th scope="col">'.$row['defect'].'</th>';
+            $defect_arr[$i] = $row['defect'];
+            echo '<th scope="col">'.$row['body'].'</th>';
+            $body_arr[$i] = $row['body'];
 
-          </tbody>
-        </table>
-      </div><!-- /.row 1 -->
 
+            $row_sum = $row['flavor'] + $row['balance'] + $row['roastlevel'] + $row['acidity'] + $row['sweetness'] + $row['aroma'] +
+            $row['aftertaste'] + $row['uniformity'] + $row['cleanup'] + $row['defect'] + $row['body'];
+            $row_avg = $row_sum / 11;
+            echo '<th scope="col">'.$row_avg.'</th>';
+            echo '</tr>';
+            $i = $i + 1;
+          }
 
-    </form>
-    <div class="footer" style="height:50px"> <div>
+          ?>
 
-    </div> <!-- container marketing -->
+        </tbody>
+      </table>
+    </div><!-- /.row 1 -->
 
+    <div class="row"> <!-- row 2-->
+      <div id="graph" style="width:90%;"></div>
+    </div>
 
-  </body>
+    <div class="footer" style="height:50px"> </div>
 
-  </html>
+  </div> <!-- container marketing -->
+  <script>
+  $.jqplot ('graph',
+  [
+    <?php
+    write_arr_to_script($flavor_arr);
+    write_arr_to_script($balance_arr);
+    write_arr_to_script($roastlevel_arr);
+    write_arr_to_script($acidity_arr);
+    write_arr_to_script($sweetness_arr);
+    write_arr_to_script($aroma_arr);
+    write_arr_to_script($aftertaste_arr);
+    write_arr_to_script($uniformity_arr);
+    write_arr_to_script($cleanup_arr);
+    write_arr_to_script($defect_arr);
+    write_arr_to_script($body_arr);
+    ?>
+  ],
+  {
+    title: 'Cupping History Graph',
+    axes: {
+      xaxis:{
+        renderer: $.jqplot.CategoryAxisRenderer,
+        label: "Saved time"
+      },
+      yaxis: {
+        label: "Evalutaion grade"
+      },
+    },
+    legend: {
+      renderer : $.jqplot.EnhancedLegendRenderer,
+      show: true, //표시 유무
+      location: 'e', // 위치 (e,w,s,n) 동서남북 조합가능 ne
+      placement: 'outside', // 위치 (Default 값은 inside)
+      textColor : 'black',
+      rowSpacing : '0px',//범례 들간의 사이 공간
+      labels:'Beverage',
+    }
+  }
+);
+</script>
+</body>
+
+</html>
